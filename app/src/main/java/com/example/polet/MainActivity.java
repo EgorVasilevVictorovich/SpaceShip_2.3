7c58c3c13e69eb86facc7ba13867465cb8535b15
EgorVasilevVictorovich <123800500+EgorVasilevVictorovich@users.noreply.github.com>
2023-04-27 19:16:46 +0300

    z

---
+++ app/src/main/java/com/example/polet/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.polet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // поля
+    private TextView highScore; // поле вывода рекордных значений
+    private boolean isMute; // поле включения и выключения звука приложения
+    private ImageView volumeOn; // поле изображения включения и выключения звука
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // определение полноэкранного режима
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // обработка нажатия на TextView с id play
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // переключение активити с помощью намерения
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
+        // присваивание id созданным переменным
+        highScore = findViewById(R.id.highScore);
+        volumeOn = findViewById(R.id.volumeOn);
+
+        // получение доступа к настройкам
+        SharedPreferences preferences = getSharedPreferences("game", MODE_PRIVATE);
+        // вывод на экран рекордного значения
+        highScore.setText("Рекорд: " + preferences.getInt("highscore",0));
+
+        // включение и выключение звука в настройках
+        // метод getBoolean("isMute", false) возвращает булевское значение по ключу "isMute", если такого нет то возвращает false
+        isMute = preferences.getBoolean("isMute", false);
+        // загрузка иконок на картинку звука
+        if (isMute) {
+            volumeOn.setImageResource(R.drawable.nosound);
+        } else {
+            volumeOn.setImageResource(R.drawable.volum);
+        }
+        // обработка нажатия на картинку звука для включения и выключения звука
+        volumeOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isMute = !isMute;
+                // загрузка иконок на картинку звука
+                if (isMute) {
+                    volumeOn.setImageResource(R.drawable.nosound);
+                } else {
+                    volumeOn.setImageResource(R.drawable.volum);
+                }
+                // внесение данных в настройки приложения
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+            }
+        });
+    }
+}
